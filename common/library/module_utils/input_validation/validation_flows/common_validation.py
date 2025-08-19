2290f9471b1ae7991e0d614a5466569ae6c9cf12
Manasa H <manasa.hemmanur@dell.com>
2025-08-19 15:47:17 +0530

    Update common_validation.py

--- common/library/module_utils/input_validation/validation_flows/common_validation.py
+++ common/library/module_utils/input_validation/validation_flows/common_validation.py
@@ -198,6 +198,8 @@ def validate_software_config(
         json_paths = get_json_file_path(
             software, cluster_os_type, cluster_os_version, input_file_path, arch_list
         )
+        if "additional_software" == software:
+            continue
         for json_path in json_paths:
             # Check if json_path is None or if the JSON syntax is invalid
             if not json_path:
